c2adea41f835a224fac8240ce1ac15f8c64d86b7
Vivian Bui <vivian30@hawaii.edu>
2025-09-30 15:31:31 -1000

    added overlay nav, navicon

--- js/script.js
+++ js/script.js
@@ -1,4 +1,19 @@
-//document content loaded
-document.addEventListener("DOMContentLoaded", function (){
-    // GLOBAL FUNCTIONS GO HERE
+document.addEventListener("DOMContentLoaded", () => {
+
+    // ENABLE MOBILE MENU 
+    const sitenav = document.querySelector(".site-nav");
+    const menubutton = document.querySelector(".menubutton");
+
+    menubutton.onclick = () => {
+    
+        // IF MENU IS CLOSED, OPEN IT, ELSE CLOSE IT
+        if (sitenav.getAttribute("data-menustate") === "closed" ) {
+            sitenav.setAttribute("data-menustate", "open");
+        } else {
+            sitenav.setAttribute("data-menustate", "closed");
+        };
+    
+    };
+
+
 });
